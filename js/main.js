8e2cca022f18446bb8c1885abf2f38a6c81241a9
ColinLynchStudent <74424172+ColinLynchStudent@users.noreply.github.com>
2020-12-14 23:24:35 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-$(".section").each(function(){
-	topPosition = $(this.offset().top
-	console.log(topPosition)
-
-	$(this).attr("data-top",topPosition)
-})
 
 
-$(document).scroll(function(){
-	ft = $(document).scrollTop();
-	console.log(ft)
+			var sectionArray = []; 
 
-	for (i=0;i<sectionArray.length;i++) {
-		if ((ft >=sectionArray [i]) && (ft < sectionArray[i+1])) {
-			var s -= "#section-"+i
-			var prev = "#section-"+(i-1)
-			var next = "#section-"+(i+1)
-			console.log(prev)
-			$(prev).css({"opacity":0})
-			$(s).css({"opacity":1})
-		}
-	}
-})
+			$(".section").each(function(){ 
+				var topPosition = $(this).offset().top 
+
+				$(this).attr("data-top",topPosition) 
+				sectionArray.push(topPosition) 
+			})
+
+
+			$(document).scroll(function(){ 	
+				
+				var windowOffset = $(window).height()/6
+				var ft = $(document).scrollTop()+windowOffset 
+
+				var i=0; 	
+				while (i<sectionArray.length) { 
+
+					if ((ft >= sectionArray[i]) && (ft < sectionArray[i+1])) {
+						var s = "#section-"+i
+						$(".section").css({"opacity":0})
+						$(s).css({"opacity":1})
+						
+					}
+					i=i+1; 
+				}
+
+			})
